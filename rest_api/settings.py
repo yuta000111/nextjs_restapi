22ac1ec951b0fddb360531a3cf4d99df251f6844
yuta000111 <yuuta0114@gmail.com>
2021-12-06 19:43:02 +0900

    added host url

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nextjs-apis-001.herokuapp.com']
 
 
 # Application definition
